d38c3d2e1d1678ebd2a589150572c9f953a7a3fc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-16 06:15:20 +0100

    Fifth Commit: Implement first iteration of LoginServlet class

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -1,40 +1,51 @@
 package webapp;
 
-
 import java.io.IOException;
-import javax.servlet.ServletException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
+/*
+ * Browser sends Http Request to Web Server
+ * 
+ * Code in Web Server => Input:HttpRequest, Output: HttpResponse
+ * JEE with Servlets
+ * 
+ * Web Server responds with Http Response
  */
+
+//Java Platform, Enterprise Edition (Java EE) JEE6
+
+//Servlet is a Java programming language class 
+//used to extend the capabilities of servers 
+//that host applications accessed by means of 
+//a request-response programming model.
+
+//1. extends javax.servlet.http.HttpServlet
+//2. @WebServlet(urlPatterns = "/login.do")
+//3. doGet(HttpServletRequest request, HttpServletResponse response)
+//4. How is the response created?
+
+@WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+	private static final long serialVersionUID = 6886933344657784107L;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Yahoo!!!!!!!!</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("My First Servlet");
+		out.println("</body>");
+		out.println("</html>");
+
 	}
 
 }
