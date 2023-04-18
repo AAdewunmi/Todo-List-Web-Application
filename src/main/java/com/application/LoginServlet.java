995c03bb18702f2d6b07453afa668c1469e54bfb
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-18 15:17:27 +0100

    Sixteenth Commit: Create skeleton UserValidationService class

--- src/main/java/com/application/LoginServlet.java
+++ src/main/java/com/application/LoginServlet.java
@@ -1,3 +1,4 @@
+package com.application;
 
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class LoginServlet extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
 		request.setAttribute("name", request.getParameter("name"));
+		request.setAttribute("password", request.getParameter("password"));
 		request.getRequestDispatcher("WEB-INF/views/Welcome.jsp").forward(request, response);
 	}
 
