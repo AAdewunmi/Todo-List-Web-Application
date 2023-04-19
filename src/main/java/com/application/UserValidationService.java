a2674c51c561b246ddb0f1fe1e2d7dd1b75caabe
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-19 07:36:15 +0100

    Seventeenth Commit: Implement User validation service

--- src/main/java/com/application/UserValidationService.java
+++ src/main/java/com/application/UserValidationService.java
@@ -1,5 +1,12 @@
 package com.application;
 
 public class UserValidationService {
+	
+	public boolean isUserValid(String user, String password) {
+		if (user.equals("user") && password.equals("12345")) {
+			return true;
+		}
+		return false;
+	}
 
 }
