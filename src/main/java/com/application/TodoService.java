01000094bdbf8c19188747599448de872982add2
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-04-24 08:38:04 +0100

    Twenty-Four: Implement static list of todo's

--- src/main/java/com/application/TodoService.java
+++ src/main/java/com/application/TodoService.java
@@ -1,5 +1,22 @@
 package com.application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.application.todo.Todo;
+
 public class TodoService {
+	
+	private static List<Todo> todos = new ArrayList<Todo>();
+	
+	static {
+		todos.add(new Todo("Learn Web Application Development"));
+		todos.add(new Todo("Learn Spring MVC"));
+		todos.add(new Todo("Learn Spring REST Services"));
+	}
+	
+	public List<Todo> retrieveTodos(){
+		return todos;
+	}
 
 }
