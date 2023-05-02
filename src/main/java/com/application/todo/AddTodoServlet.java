1e2d97e0458f520f737c8bf496f781cfae9815df
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-02 15:18:55 +0100

    Forty-Six Commit: Create skeleton AddTodoServlet class

---
+++ src/main/java/com/application/todo/AddTodoServlet.java
@@ -0,0 +1,35 @@
+package com.application.todo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TodoServlet
+ */
+
+@WebServlet("/todo.do")
+public class AddTodoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private TodoService todoService = new TodoService();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.setAttribute("todos", todoService.retrieveTodos());
+		request.getRequestDispatcher("WEB-INF/views/todo.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String newTodo = request.getParameter("todo");
+		todoService.addTodo(new Todo(newTodo));
+		response.sendRedirect("/todo.do");
+	}
+	
+	
+	
+
+}
