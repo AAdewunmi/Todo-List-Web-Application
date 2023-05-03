bab2403e1e2d3ff5dc94070990499012daf00f86
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-03 16:45:13 +0100

    Fifty-Four: Implement logout functionality

--- src/main/java/com/application/logout/LogoutServlet.java
+++ src/main/java/com/application/logout/LogoutServlet.java
@@ -1,6 +1,5 @@
 package com.application.logout;
 
-
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -9,34 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.application.todo.TodoService;
 
-@WebServlet(urlPatterns = "/login.do")
+
+@WebServlet(urlPatterns = "/logout.do")
 public class LogoutServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 6886933344657784107L;
-	LoginService userValidationService = new LoginService();
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		request.getRequestDispatcher("WEB-INF/views/Login.jsp").forward(request, response);
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
-		String name = request.getParameter("name");
-		String password = request.getParameter("password");
-		
-		boolean isUserValid = userValidationService.isUserValid(name, password);
-		
-		if (isUserValid) {
-			request.getSession().setAttribute("name", name);
-			response.sendRedirect("/todo.do");
-		}else {
-			request.setAttribute("errorMessage", "Invalid Credentials!");
-			request.getRequestDispatcher("WEB-INF/views/Login.jsp").forward(request, response);
-		}
-		
-		
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getSession().invalidate();
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 
+	}
 }
