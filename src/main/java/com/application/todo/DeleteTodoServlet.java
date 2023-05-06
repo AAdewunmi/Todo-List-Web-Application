4285c860240660fd096179f799b892184c63896d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-05-06 11:13:46 +0100

    Sixty-Three Commit: Implement new todo category functionality in DeleteTodoServlet class

--- src/main/java/com/application/todo/DeleteTodoServlet.java
+++ src/main/java/com/application/todo/DeleteTodoServlet.java
@@ -15,7 +15,7 @@ public class DeleteTodoServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		todoService.deleteTodo(new Todo(request.getParameter("todo")));
+		todoService.deleteTodo(new Todo(request.getParameter("todo"), request.getParameter("catgory")));
 		response.sendRedirect("/list-todos.do");
 	}	
 
